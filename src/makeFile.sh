95b42b4378de1f091c77570bad17803e7f54071f
Aries3D <drdurczok@gmail.com>
2016-05-18 09:01:16 +0200

    replaced with makefile

--- src/makeFile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-    "g++ -o outPut main.cpp Listen.cpp Listen.h \
-    -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
-    `pkg-config --cflags --libs pocketsphinx sphinxbase`"
